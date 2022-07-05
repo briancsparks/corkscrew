3d9860296540390825ef40970959fac36ccc2572
Brian C Sparks <briancsparks@gmail.com>
2022-07-04 18:33:08 -0700

    Refactoring

--- corkscrew/subtile.go
+++ corkscrew/subtile.go
@@ -1,8 +1,8 @@
 package corkscrew
 
-import "image"
-
 /* Copyright © 2022 Brian C Sparks <briancsparks@gmail.com> -- MIT (see LICENSE file) */
+
+import "image"
 
 type SubTilizer struct {
   Field           *Field
@@ -27,8 +27,8 @@ func (st *SubTilizer) Curr() (*Vec2, *image.Point) {
     return nil, nil
   }
 
-  x := st.halfCoord(p.Min.X, p.Max.X, p.Rect.Min.X, p.Rect.Max.X, pt.X, false)
-  y := st.halfCoord(p.Min.Y, p.Max.Y, p.Rect.Min.Y, p.Rect.Max.Y, pt.Y, st.Field.ShowMathy)
+  x := halfCoord(p.Min.X, p.Max.X, p.Rect.Min.X, p.Rect.Max.X, pt.X, false)
+  y := halfCoord(p.Min.Y, p.Max.Y, p.Rect.Min.Y, p.Rect.Max.Y, pt.Y, st.Field.ShowMathy)
 
   st.CurrentIndexPoint = pt
 
@@ -50,15 +50,28 @@ func (st *SubTilizer) Next() (*Vec2, *image.Point) {
     return nil, nil
   }
 
-  x := st.halfCoord(p.Min.X, p.Max.X, p.Rect.Min.X, p.Rect.Max.X, pt.X, false)
-  y := st.halfCoord(p.Min.Y, p.Max.Y, p.Rect.Min.Y, p.Rect.Max.Y, pt.Y, st.Field.ShowMathy)
+  x := halfCoord(p.Min.X, p.Max.X, p.Rect.Min.X, p.Rect.Max.X, pt.X, false)
+  y := halfCoord(p.Min.Y, p.Max.Y, p.Rect.Min.Y, p.Rect.Max.Y, pt.Y, st.Field.ShowMathy)
 
   st.CurrentIndexPoint = pt
 
   return &Vec2{x, y}, &pt
 }
 
-func (st *SubTilizer) halfCoord(fmin, fmax float32, imin, imax int, midpt int, backwards bool) float32 {
+func Coordinate(t *Tile, pt image.Point, mathy bool) Vec2 {
+  x, y := Coordinate4(t.Min, t.Max, &t.Rect, pt, mathy)
+  return V2(x, y)
+}
+
+// Coordinate4 computes the center of a pixel as a float
+func Coordinate4(min, max Vec2, rect *image.Rectangle, pt image.Point, mathy bool) (float32, float32) {
+  fx := halfCoord(min.X, max.X, rect.Min.X, rect.Max.X, pt.X, false)
+  fy := halfCoord(min.Y, max.Y, rect.Min.Y, rect.Max.Y, pt.Y, mathy)
+
+  return fx, fy
+}
+
+func halfCoord(fmin, fmax float32, imin, imax int, midpt int, backwards bool) float32 {
   widthOfIcell, half, offset := computeHalfHelper(fmin, fmax, imin, imax, midpt)
 
   var center float32
@@ -71,14 +84,16 @@ func (st *SubTilizer) halfCoord(fmin, fmax float32, imin, imax int, midpt int, b
   return center
 }
 
-func (st *SubTilizer) Coord1d(fmin, fmax float32, imin, imax, midpt int) (widthOfIcell, half float32, offset int) {
+
+func computeHalfHelper(fmin, fmax float32, imin, imax int, index int) ( cellwidth, half float32, offset int) {
   fwidth := fmax - fmin
   iwidth := imax - imin
-  widthOfIcell = fwidth / float32(iwidth)
-  half = widthOfIcell / 2.0
-  offset = midpt - imin
+  cellwidth = fwidth / float32(iwidth)        // An int cell is this much wide in the float numbers
+  half = cellwidth / 2.0
+  offset = index - imin
 
-  return widthOfIcell, half, offset
+  return
 }
+
 
 
